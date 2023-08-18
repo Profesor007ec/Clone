c9bcbd3b11c5583982a6965d2934f6cb3bcef9bf
javico1981 <javico1981@hotmail.com>
2023-08-17 20:27:43 -0500

    Cors especialidades

--- src/main/java/com/company/planning/backend/controller/EspecialidadRestController.java
+++ src/main/java/com/company/planning/backend/controller/EspecialidadRestController.java
@@ -2,6 +2,7 @@ package com.company.planning.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import com.company.planning.backend.model.Especialidad;
 import com.company.planning.backend.response.EspecialidadResponseRest;
 import com.company.planning.backend.service.IEspecialidadService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/planillaje")
 public class EspecialidadRestController {
